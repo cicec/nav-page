b8d2547fcd8067cac6a6d65c5c80e14aadfeb8d1
clancysong <clancysong@gmail.com>
2018-02-13 21:15:48 +0800

    reconsitution

--- main.js
+++ main.js
@@ -1,63 +1,71 @@
-//  function
+const createNode = (tag, attributes) => {
+    const node = document.createElement(tag)
+    Object.keys(attributes).forEach((key) => {
+        node[key] = attributes[key]
+    })
+    return node
+}
+
 const createIcon = (website) => {
-    const icon = document.createElement('img')
-    icon.className = 'icon'
-    icon.src = `http://${website}/favicon.ico`
-    icon.onerror = () => {
-        icon.src = ''
+    const icon = createNode('img', { className: 'icon' })
+    if (website) {
+        icon.src = `http://${website}/favicon.ico`
+        icon.onerror = () => {
+            icon.src = '1.png'
+        }
+    } else {
+        icon.src = '1.png'
     }
     return icon
 }
 
+const main = () => {
+    const keys = [
+        ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+        ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+        ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+    ]
+    let websites = {
+        q: 'qq.com',
+        e: 'ele.me',
+        t: 'taobao.com',
+        y: 'yy.com',
+        b: 'baidu.com',
+        i: 'iqiyi.com',
+        z: 'zhihu.com'
+    }
 
-const keys = [
-    ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-    ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-    ['z', 'x', 'c', 'v', 'b', 'n', 'm']
-]
+    const websitesInLocal = JSON.parse(localStorage.getItem('websites'))
+    if (websitesInLocal) {
+        websites = websitesInLocal
+    }
 
-let websites = {
-    q: 'qq.com',
-    e: 'ele.me',
-    t: 'taobao.com',
-    y: 'yy.com',
-    b: 'baidu.com',
-    i: 'iqiyi.com'
-}
-
-const websitesInLocal = JSON.parse(localStorage.getItem('websites'))
-
-if (websitesInLocal) {
-    websites = websitesInLocal
-}
-
-for (let i = 0; i < keys.length; i++) {
-    const row = document.createElement('div')
-    row.className = 'row'
-    keyboard.appendChild(row)
-    for (let j = 0; j < keys[i].length; j++) {
-        const key = document.createElement('kbd')
-        key.className = 'key'
-        key.textContent = keys[i][j]
-
-        const edit = document.createElement('button')
-        edit.className = 'edit'
-        edit.id = keys[i][j]
-        edit.textContent = '编辑'
-        edit.onclick = (event) => {
-            const site = prompt('请输入要设置为导航的地址')
-            websites[event.target.id] = site
-            key.appendChild(createIcon(site))
-            localStorage.setItem('websites', JSON.stringify(websites))
+    for (let i = 0; i < keys.length; i++) {
+        const keyboard = document.getElementById('keyboard')
+        const row = createNode('div', { className: 'row' })
+        keyboard.appendChild(row)
+        for (let j = 0; j < keys[i].length; j++) {
+            const key = createNode('kbd', { className: 'key', textContent: keys[i][j] })
+            const edit = createNode('button', {
+                className: 'edit',
+                id: keys[i][j],
+                textContent: '编辑',
+                onclick: (event) => {
+                    const site = prompt('请输入要设置为导航的地址')
+                    websites[event.target.id] = site
+                    key.appendChild(createIcon(site))
+                    localStorage.setItem('websites', JSON.stringify(websites))
+                }
+            })
+            key.appendChild(createIcon(websites[keys[i][j]]))
+            key.appendChild(edit)
+            row.appendChild(key)
         }
+    }
 
-        if (websites[keys[i][j]]) key.appendChild(createIcon(websites[keys[i][j]]))
-
-        key.appendChild(edit)
-        row.appendChild(key)
+    document.onkeypress = (event) => {
+        window.open(`http://${websites[event.key]}`)
     }
 }
 
-document.onkeypress = (event) => {
-    window.open(`http://${websites[event.key]}`)
-}
+window.onload = main
